05852bb53259ede0ac04dee4e63ba7c74f552e56
Stefan Boila <dumitru.boila@gmail.com>
2019-01-18 15:29:24 +0200

    Added a Redis delete keys helper

---
+++ app.py
@@ -0,0 +1,29 @@
+import redis
+import os
+
+print("Hello {}!".format(os.environ.get("USER")))
+
+host = raw_input("Give a Redis host (127.0.0.1): ") or "127.0.0.1"
+port = raw_input("Give me its port (6379): ") or "6379"
+
+try:
+    r = redis.StrictRedis(host=host, port=port, db=0)
+    print("Connected to {}".format(r.connection_pool.pid))
+except Exception as e:
+    print("Couldn't connect Redis server. Error: ".format(e.message))
+    exit(1)
+
+# Delete all keys without a TTL to expire and not accessed in the last 6 months
+deleted_keys = 0
+for key in r.scan_iter("*"):
+     idle = r.object("idletime", key)
+
+     if r.ttl(key) == -1 and idle > 3600 * 24 * 30 * 6:
+        #r.delete(key)
+        print(key)
+        deleted_keys += 1
+
+if deleted_keys:
+    print("Deleted {} keys.".format(deleted_keys))
+else:
+    print("There are no key that have to be deleted")
